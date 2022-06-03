7718e26d444a9170b2e0b29299fbfff1175c5320
oyenkachi <calebononobgu@yahoo.com>
2022-06-03 12:07:47 -0700

    fist commit

---
+++ Array-method.js
@@ -0,0 +1,94 @@
+///// ARRAY MEETHODS
+
+const fruits = ["apple", "orange"]
+console.log(fruits.length);
+console.log(fruits);
+
+// const fruits = ["apple", "orange", "cherry"]
+// console.log(fruits.length);
+// console.log(fruits);
+
+// const fruits = ["apple", "orange", "cherry"]
+// console.log(fruits.length);
+// fruits[2] = "mango";
+// console.log(fruits);
+
+// const fruits = ["apple", "orange", "cherry"]
+// console.log(fruits.length);
+// fruits[2] 
+// console.log(fruits);
+
+/// PUSH
+// const fruits = ["apple", "orange", "cherry"];
+
+// fruits.push("strawberry");
+// console.log(fruits.length);
+// console.log(fruits);
+
+/// SPEARD OPERATORS
+ 
+// const person = {
+//     name: "victor",
+//     age: 12,
+//     nigerian: true,
+// };
+
+// const newPerson ={...person};
+// console.log(newPerson);
+
+// newPerson.name = "caleb";
+// console.log(newPerson);
+// console.log(person);
+
+// delete person.age
+
+// console.log(person);
+
+/// POP
+// const fruits = ["apple", "orange", "cherry"];
+
+// console.log(fruits);
+// fruits.pop ();
+// console.log(fruits);
+
+// const fruits = ["apple", "orange", "cherry"];
+
+// console.log(fruits);
+// let poppedFruit = fruits.pop();
+// fruits.pop ();
+// console.log(fruits);
+// console.log(poppedFruit);
+
+/// UNSHIFT
+
+// const fruits = ["apple", "orange", "cherry"];
+
+
+// console.log(fruits);
+// // ..fruits.unshift()
+// let newSize = fruits.unshift("papaya");
+// console.log(newSize);
+// console.log(fruits);
+
+/// SHIFT
+// const fruits = ["apple", "orange", "cherry"];
+
+// let removedFruit = fruits.shift();
+// console.log(removedFruit);
+// console.log(fruits);
+
+/// SPLICE => IT CAN DO ANYTHING: INSERT, REMOVE AND REPLACE ELEMENTS.
+
+// let arr = ["I", "study", "javascript", "right", "now"];
+// // arr.splice("Let", "dance");
+// arr.splice(0, 3, "Let", "dance");
+// console.log(arr);
+
+/// CONCAT => IT CREATES NEW ARRAY THAT INCLUDES VALUE FROM OTHER ARRAYS AND ADDITIONAL ITEMS
+// IT ACCEPT ANY NUMBER OG ARGUMENT --> EITHER ARRAYS OR VALUES.
+
+// let arr = [1, 2];
+
+// console.log(arr.concat([3, 4]));
+// console.log(arr.concat([5, 6, 7]));
+// console.log(arr.concat([12, 20]));
